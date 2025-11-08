17b38cbf1cd9117285244f2a0d5eeb0de97a3b0e
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2025-11-08 12:54:54 +0600

    Logo changed to default dark and light, and agent page now main page

--- src/components/partials/logo/index.jsx
+++ src/components/partials/logo/index.jsx
@@ -1,12 +1,4 @@
 "use client";
-import enterprise_dark_logo from "@/../public/logos/enterprise_dark_logo.svg";
-import enterprise_plan_logo from "@/../public/logos/enterprise_plan_logo.svg";
-import pro_dark_logo from "@/../public/logos/pro_dark_logo.svg";
-import pro_plan_log from "@/../public/logos/pro_plan_logo.svg";
-import unlimited_dark_logo from "@/../public/logos/unlimited_dark_logo.svg";
-import unlimited_plan_logo from "@/../public/logos/unlimited_plan_logo.svg";
-import value_dark_logo from "@/../public/logos/value_dark_logo.svg";
-import value_plan_logo from "@/../public/logos/value_plan_logo.svg";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
@@ -16,28 +8,9 @@ import { useSelector } from "react-redux";
 const Logo = forwardRef(({ className }, ref) => {
   const { user } = useSelector((state) => state.auth);
   const { theme } = useSelector((state) => state.settings);
-  const isDark = theme === "dark";
+  const isDark = theme === "dark" || theme === "system";
 
-  const logoSrc =
-    user?.package === "pro_plan"
-      ? isDark
-        ? pro_dark_logo
-        : pro_plan_log
-      : user?.package === "value_plan"
-        ? isDark
-          ? value_dark_logo
-          : value_plan_logo
-        : user?.package === "unlimited"
-          ? isDark
-            ? unlimited_dark_logo
-            : unlimited_plan_logo
-          : user?.package === "enterprise"
-            ? isDark
-              ? enterprise_dark_logo
-              : enterprise_plan_logo
-            : isDark
-              ? "/shothik_dark_logo.png"
-              : "/shothik_light_logo.png";
+  const logoSrc = isDark ? "/shothik_dark_logo.png" : "/shothik_light_logo.png";
 
   return (
     <Link
